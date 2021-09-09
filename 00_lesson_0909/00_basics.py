e290a03a678d19989c49e152fc63fa2e7ec90079
demidraksin <greatraksin@icloud.com>
2021-09-09 11:23:00 +0300

    IInitial commit⭐️

---
+++ 00_lesson_0909/00_basics.py
@@ -0,0 +1,10 @@
+x = 10 + 2.5  # cоздал переменную
+print(x)  # вывел значение переменной
+print(type(x))  # вывел тип данных переменной
+
+age = input('Введите свой возраст: ')
+print('age строка')
+print(type(age))
+age = int(age)
+print('age число')
+print(type(age))
